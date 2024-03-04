d6e0703e078b1f2995dfc1a3721f63f4d7e88fc8
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-03-05 02:12:25 +0530

    header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Container from "../container/Container"
-import Logo from "../Logo"
 import {Link} from "react-router-dom"
 import LogoutBtn from './LogoutBtn'
 import { useNavigate } from 'react-router-dom'
